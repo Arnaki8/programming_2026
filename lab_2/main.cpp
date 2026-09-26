94c9da72112420fbbe0e61fd01b42bf7b9b5956e
Arnaki8 <ksenagaranina@gmail.com>
2026-09-26 08:47:30 +0000

    vzв

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -2,15 +2,39 @@
 #include <limits>
 
 
+bool isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+
 int main() {
     
     int n = 0;
+    std::cout << "Введите число" << std::endl;
     std::cin >> n;
+
+    
     if (n % 2 == 0) {
-        std::cout << "Even";
+        std::cout << "Четное" << std::endl;
     } else {
-        std::cout << "Odd";
+        std::cout << "Нечетное" <<std::endl;
     }
     
+
+    if (isPrime(n)) {
+        std::cout << "Простое" << std::endl;
+    } else {
+        std::cout << "Сложное" <<std::endl;
+    }
+
+
+
+
+
     return 0;
 }
